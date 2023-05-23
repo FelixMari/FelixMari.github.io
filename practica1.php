749663a77c3fa06a349edddb0e0e6a58506abc4e
Mariana Felix <124410469+FelixMari@users.noreply.github.com>
2023-05-22 18:57:13 -0600

    Add files via upload

---
+++ practica1.php
@@ -0,0 +1,15 @@
+<?php
+echo "<h1>Hola papus</h1>";
+$nombre = "mari uwu";
+echo "<hr>";
+$edad = 16;
+if($edad >= 18){
+    echo $nombre . " eres mayor de edad";
+} else {
+    echo $nombre . " eres menor de edad";
+}
+echo "<hr>";
+for($i = 0; $i <10; $i++){
+    echo $i . "<br>";
+}
+?>
