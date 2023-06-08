8a3ab4be9bde0a4935e7e60f2d1a9676d15ad302
Mariana Felix <124410469+FelixMari@users.noreply.github.com>
2023-06-07 19:24:14 -0600

    Add files via upload

--- PARCIAL3/practica5.php
+++ PARCIAL3/practica5.php
@@ -14,9 +14,11 @@ for ($i=0; $i<count($cuadros); $i++){
 */
 if($cuadros[0] =="X" && $cuadros[1] =="X" && $cuadros[2] =="X"){
     echo"<h1> Ganador X </h1>";
+    echo LineaGanadora(0, 1, 2, $cuadros);
     return;
 }else if ($cuadros[0] =="O" && $cuadros[1] =="O" && $cuadros[2] =="O"){
     echo"<h1> Ganador O </h1>";
+    echo LineaGanadora(0, 1, 2, $cuadros);
     return;
 }
 /*
@@ -26,9 +28,11 @@ if($cuadros[0] =="X" && $cuadros[1] =="X" && $cuadros[2] =="X"){
 */
 if($cuadros[3] =="X" && $cuadros[4] =="X" && $cuadros[5] =="X"){
     echo"<h1> Ganador X </h1>";
+    echo LineaGanadora(3, 4, 5, $cuadros);
     return;
 }else if ($cuadros[3] =="O" && $cuadros[4] =="O" && $cuadros[5] =="O"){
     echo"<h1> Ganador O </h1>";
+    echo LineaGanadora(3, 4, 5, $cuadros);
     return;
 }
 /*
@@ -38,9 +42,11 @@ if($cuadros[3] =="X" && $cuadros[4] =="X" && $cuadros[5] =="X"){
 */
 if($cuadros[6] =="X" && $cuadros[7] =="X" && $cuadros[8] =="X"){
     echo"<h1> Ganador X </h1>";
+    echo LineaGanadora(6, 7, 8, $cuadros);
     return;
 }else if ($cuadros[6] =="O" && $cuadros[7] =="O" && $cuadros[8] =="O"){
     echo"<h1> Ganador O </h1>";
+    echo LineaGanadora(6, 7, 8, $cuadros);
     return;
 }
 /*
@@ -50,9 +56,11 @@ if($cuadros[6] =="X" && $cuadros[7] =="X" && $cuadros[8] =="X"){
 */
 if($cuadros[0] =="X" && $cuadros[3] =="X" && $cuadros[6] =="X"){
     echo"<h1> Ganador X </h1>";
+    echo LineaGanadora(0, 3, 6, $cuadros);
     return;
 }else if ($cuadros[0] =="O" && $cuadros[3] =="O" && $cuadros[6] =="O"){
     echo"<h1> Ganador O </h1>";
+    echo LineaGanadora(0, 3, 6, $cuadros);
     return;
 }
 /*
@@ -62,9 +70,11 @@ if($cuadros[0] =="X" && $cuadros[3] =="X" && $cuadros[6] =="X"){
 */
 if($cuadros[1] =="X" && $cuadros[4] =="X" && $cuadros[7] =="X"){
     echo"<h1> Ganador X </h1>";
+    echo LineaGanadora(1, 4, 8, $cuadros);
     return;
 }else if ($cuadros[1] =="O" && $cuadros[4] =="O" && $cuadros[7] =="O"){
     echo"<h1> Ganador O </h1>";
+    echo LineaGanadora(1, 4, 8, $cuadros);
     return;
 }
 /*
@@ -74,9 +84,11 @@ if($cuadros[1] =="X" && $cuadros[4] =="X" && $cuadros[7] =="X"){
 */
 if($cuadros[2] =="X" && $cuadros[5] =="X" && $cuadros[8] =="X"){
     echo"<h1> Ganador X </h1>";
+    echo LineaGanadora(2, 5, 8, $cuadros);
     return;
 }else if ($cuadros[2] =="O" && $cuadros[5] =="O" && $cuadros[8] =="O"){
     echo"<h1> Ganador O </h1>";
+    echo LineaGanadora(2, 5, 8, $cuadros);
     return;
 }
 /*
@@ -86,9 +98,11 @@ if($cuadros[2] =="X" && $cuadros[5] =="X" && $cuadros[8] =="X"){
 */
 if($cuadros[0] =="X" && $cuadros[4] =="X" && $cuadros[8] =="X"){
     echo"<h1> Ganador X </h1>";
+    echo LineaGanadora(0, 4, 8, $cuadros);
     return;
 }else if ($cuadros[0] =="O" && $cuadros[4] =="O" && $cuadros[8] =="O"){
     echo"<h1> Ganador O </h1>";
+    echo LineaGanadora(0, 4, 8, $cuadros);
     return;
 }
 /*
@@ -98,9 +112,22 @@ if($cuadros[0] =="X" && $cuadros[4] =="X" && $cuadros[8] =="X"){
 */
 if($cuadros[2] =="X" && $cuadros[4] =="X" && $cuadros[6] =="X"){
     echo"<h1> Ganador X </h1>";
+    echo LineaGanadora(2, 4, 6, $cuadros);
     return;
 }else if ($cuadros[2] =="O" && $cuadros[4] =="O" && $cuadros[6] =="O"){
     echo"<h1> Ganador O </h1>";
+    echo LineaGanadora(2, 4, 6, $cuadros);
     return;
+}
+function LineaGanadora($p1, $p2, $p3, $cuadros){
+    $color = 'background-color: pink; border: 1px solid black; padding: 2px;';
+    for($i=0; $i<9; $i++){
+        if($i == $p1 || $i == $p2 || $i == $p3){
+            echo "<span style= 'color: black;".$color."'>".$cuadros[$i]."</span>";
+        }else {
+            echo"<span>".$cuadros[$i]."</span>";
+        }
+        if($i == 2 || $i == 5)echo "<br>";
+    }
 }
 ?>
