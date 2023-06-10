cc42a345083e8756935f184cb310a2570d6387df
Mariana Felix <124410469+FelixMari@users.noreply.github.com>
2023-06-09 20:16:18 -0600

    Add files via upload

--- PARCIAL3/practica6.php
+++ PARCIAL3/practica6.php
@@ -7,13 +7,44 @@
     <title>Juego del ahorcado</title>
     <link rel="stylesheet" href="css/bootstrap.css">
 </head>
-<body>
+<body style="background-color: pink; ">
     <?php
     $nombre = $_POST["jugador"];
     $palabra = $_POST["palabra"];
     $vidas = $_POST["vidas"];
-    ?>
+    $b = false;
+    $frase = $_POST["frase"];
+
+    if(isset($_POST["frase"])){
+        $frase = $_POST["frase"];
+        $letra=$_POST["letra"];
+        for($i=0 ; $i<count(str_split($palabra));$i++){
+            if($palabra[$i] == $letra){
+                $frase[$i] = $letra;
+                $b = true; } }
+        if(!$b){
+            echo "<h4>La letra -".$letra."- no está en la palabra :b</h4>";
+            $vidas--; }
+        if($vidas <= 0){
+            echo "<h1>Perdiste, ¡se agotaron tus vidas!</h1>";
+            echo "<img src='gameover.png' width='15%'><hr>";
+            echo "<a href='practica6.html'>Regresar al inicio</a>";
+            return; }
+        $terminado = false;
+        for($j=0 ; $j<count(str_split($palabra)); $j++){
+            if(!$frase[$j]){
+                $terminado = true; } }
+        if($terminado == false){
+            echo "<h1>¡Ganaste!, eres un genio.</h1>";
+            echo "<img src='ganador.png' width='15%'><hr>";
+            echo "<a href='practica6.html'>Regresar al inicio</a>";
+            return; } } ?>
     <div class="container">
+    <form action="practica6.php" method="POST">
+            <input type="hidden" value="<?php echo $nombre ?>" name="jugador">
+            <input type="hidden" value="<?php echo $palabra ?>" name="palabra">
+            <input type="hidden" value="<?php echo $vidas ?>" name="vidas">
+
         <div class="row">
             <h1>Juego del ahorcado</h1><hr>
             <h3>Nombre del jugador: <?php echo $nombre; ?> </h3>
@@ -21,12 +52,14 @@
             <?php
             $letras = str_split($palabra);
             for($i=0; $i<(count($letras)); $i++){
-                echo "<input type='text' name='frase[]' style='width: 5%;'>";}
+                echo "<input type='text' name='frase[]' style='width: 5%;' value='".$frase[$i]."'>";}
             ?>
         </div>
             <div class="row mt-4">
-                <input type="text" name="letra" style="width: 10%;">
-                <input type="submit" class="btn btn-primary" value="Enviar letra" style="width: 10%;">
+                <input type="text" name="letra" style="width: 10%;" name="letra" maxlength="1" required>
+            </div>
+            <div>
+                <input type="submit" class="btn btn-primary" value="Enviar letra" style="width: 10%; background-color: brown;">
             </div>
     </div>
 </body>
